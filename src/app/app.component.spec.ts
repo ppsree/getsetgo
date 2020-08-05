4ad0547772440a90e81f3b98b6e327661e510a01
Sreelakshmi <ppsree1790@gmail.com>
2020-08-05 19:27:38 +0530

    Add initial source code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'planner'`, () => {
+  it(`should have as title 'getsetgo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('planner');
+    expect(app.title).toEqual('getsetgo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('planner app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('getsetgo app is running!');
   });
 });
